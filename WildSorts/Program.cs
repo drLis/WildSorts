c6b413e386dc15353c36abfe79599bf7742825b5
dr.lis <dr.lis@outlook.com>
2020-04-17 22:36:19 +0300

    Completed sort with O(nlogk)

--- WildSorts/Program.cs
+++ WildSorts/Program.cs
@@ -69,7 +69,12 @@ namespace WildSorts
 				if (node!= null)
 				{
 					var array = ToArray(node.left);
-					array.AddRange(new List<Int32>(node.counter) { node.value });
+					var m = new List<Int32>();
+					for (Int32 i = 0; i < node.counter; i++)
+					{
+						m.Add(node.value);
+					}
+					array.AddRange(m);
 					array.AddRange(ToArray(node.right));
 					return array;
 				}
@@ -80,9 +85,24 @@ namespace WildSorts
 			}
 		}
 
+		static Int32[] sort(Int32[] array)
+		{
+			BinaryTree tree = new BinaryTree();
+			for (Int32 i = 0; i < array.Length; i++)
+			{
+				tree.Insert(array[i], tree.Root);
+			}
+			return tree.ToArray(tree.Root).ToArray();
+		}
+
 		static void Main(string[] args)
 		{
-			
+			Int32[] array = new Int32[] { 1, 3, 5, 7, 9, 0, 8, 6, 4, 2, 1, 1, 2, 3, 5, 8, 13 };
+			var sortedArray = sort(array);
+			foreach (var i in sortedArray)
+			{
+				Console.WriteLine(i);
+			}
 		}
 	}
 }
